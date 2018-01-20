d26f94e813f434115f9d64745ab8e3dff52be42b
ishu2 <rathiishita123@gmail.com>
2018-01-21 01:57:42 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,105 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var mongoose=require('mongoose');
+var methodOverride=require('method-override');
+var expressSanitizer=require('express-sanitizer');
+var app=express();
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(expressSanitizer());
+mongoose.connect('mongodb://localhost/blog_app');
+
+var blogSchema=new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+var Blog=mongoose.model("Blog",blogSchema);
+
+app.get('/',function(req,res){
+    res.redirect('/blogs');
+});
+
+// INDEX ROUTE
+app.get('/blogs',function(req,res){
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log("ERROR!!!");
+        }else{
+            res.render('index',{blogs:blogs});
+        }
+    })
+})
+
+// NEW ROUTE
+app.get('/blogs/new',function(req,res){
+    res.render('new');
+});
+
+// CREATE ROUTE
+app.post('/blogs',function(req,res){
+    req.body.blog.body=req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog,function(err,newBlog){
+        if(err){
+            res.render('new');
+        }else{
+            res.redirect('/blogs');
+        }
+    })
+})
+
+//SHOW ROUTE
+app.get('/blogs/:id',function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        }else{
+            res.render('show',{blog:foundBlog});
+        }
+    })
+})
+
+//EDIT ROUTE
+app.get('/blogs/:id/edit',function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        }else{
+            res.render('edit',{blog:foundBlog});
+        }
+    })
+})
+
+//UPDATE ROUTE
+app.put('/blogs/:id',function(req,res){
+   Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+       if(err){
+           res.redirect('/blogs');
+       }else{
+           res.redirect('/blogs/'+req.params.id);
+       }
+   })
+})
+
+// DELETE ROUTE
+app.delete('/blogs/:id',function(req,res){
+    Blog.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.redirect('/blogs');
+        }else{
+            res.redirect('/blogs');
+        }
+    })
+})
+
+app.listen(1000,function(){
+    console.log("Server started..");
+})
